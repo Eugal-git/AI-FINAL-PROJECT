d19bc18a8734152ec40dd25f2d5a009260ee6f5e
Eugal-git <eugeniatunha06@gmail.com>
2024-10-18 16:10:25 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
+function displayAdvice(response) {
+  new Typewriter("#advice", {
+    strings: response.data.answer,
+    autoStart: true,
+    cursor: "",
+    delay: 20,
+  });
+}
+
+function generateAdvice(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#location");
+  let apiKey = "dd9ad0073b1c71eoafbt4187fa1bef0d";
+  let context =
+    "You are a very well travelled AI and have been to every country in the world. Please give eight recommendetion on things to see and do and write this in basic HTML. Include a title in <h1> and include each suggestion below with the headline of the suggestions in <h2> and then further instructions below as body text. Do not put ```html at the beginning or ``` at the end. Please follow the user instructions";
+  let prompt = `User intsructions: Generate recommendations on travelling to ${instructionsInput.value}`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let adviceElement = document.querySelector("#advice");
+  adviceElement.classList.remove("hidden");
+  adviceElement.innerHTML = `<div class="generating"> ⌛ Generating Travel Advice about ${instructionsInput.value}</div>`;
+
+  axios.get(apiUrl).then(displayAdvice);
+}
+
+let travelFormElement = document.querySelector("#travel-generator-form");
+travelFormElement.addEventListener("submit", generateAdvice);
 
